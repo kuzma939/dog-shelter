2a86bbbd054255bc847d0fed1b6a5338870694de
oleksii_ozyrskyi <aozyrskyii@gmail.com>
2025-09-01 13:38:31 +0200

    add| header + footer base

---
+++ app/components/Footer/Footer.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import css from "./Footer.module.css";
+import { FaPaw } from "react-icons/fa";
+
+export default function Footer() {
+  return (
+    <footer className={css.footer}>
+      <div className="container">
+        <div className={css.wrapper}>
+          <div>
+            <p>Пн-Нд</p>
+            <p>8:00-21:00</p>
+            <br />
+            <a
+              href="https://www.google.com/maps/place/Пр.+Академіка+Глушкова,+1,+Київ,+03137"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Пр. Академія Глушкова 1, Київ 03137
+            </a>
+          </div>
+          <div>
+            <FaPaw className={css.paw} />
+          </div>
+          <div>
+            <p>Телефон</p>
+
+            <p>066 3266122</p>
+            <br />
+
+            <a href="mailto:pet.center@gmail.com">pet.center@gmail.com</a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
