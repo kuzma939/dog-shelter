a2f16efdf2716605bda3cb2134f4ca6f24c5b573
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2025-09-02 10:00:37 +0200

    2

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import css from "./Header.module.css";
 import { GiHamburgerMenu } from "react-icons/gi";
-
-export default function Header() {
+export default function Header({ overlay = false }) {
   return (
-    <header className="container">
-      <div className={css.div}>
+    <header className={overlay ? css.overlay : ""}>
+      <div className={`container ${css.div}`}>
+
         <button className={css.button}>Галерея</button>
         <button className={css.button}>Відгуки</button>
         <button className={css.button}>Про нас</button>
