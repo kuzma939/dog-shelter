baf5c1770f16ff7fc21d68a174db65b4aecbb86e
oleksii_ozyrskyi <aozyrskyii@gmail.com>
2025-09-03 12:37:57 +0200

    add| faq page

--- src/app/page.js
+++ src/app/page.js
@@ -2,16 +2,14 @@ import Image from "next/image";
 import Hero from "../components/Hero/Hero.js";
 import HowToHelp from "../components/HowToHelp/HowToHelp.jsx";
 import Address from "../components/Address/Address.jsx";
-import AnimalsBar from "../components/AnimalsBar/AnimalsBar.js"
+import AnimalsBar from "../components/AnimalsBar/AnimalsBar.js";
 export default function Home() {
   return (
- <main className="flex flex-col gap-0">
-        <Hero />
-        < AnimalsBar />
-       
-        <HowToHelp />
-      </main>
- 
-    
+    <main className="flex flex-col gap-0">
+      <Hero />
+      <AnimalsBar />
+      <HowToHelp />
+      <Address />
+    </main>
   );
 }
